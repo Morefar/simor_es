cef78211d7ae54fd825f3baa4206bbe290d03e98
Fernando Angarita <fangari@gmail.com>
2013-05-14 20:10:49 -0500

    Adds asset model required fields to comply with regulation.

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,7 +1,7 @@
 class Asset < ActiveRecord::Base
   belongs_to :contract, inverse_of: :assets
   attr_accessible :contract_id, :invoice_id, :inventory_number, :license_plate,
-                :chassis_number, :motor_number, :cylinder_cap, :capacity,
+                :chassis_number, :motor_number, :horse_power, :capacity,
                 :capacity_unit, :make, :model, :year, :color, :build, :service_type,
                 :service_mode, :fuel_type, :book_value
 
