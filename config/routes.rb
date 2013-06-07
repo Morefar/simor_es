68846c6727de4fc3d400e1208926ac1e404830c1
Fernando Angarita <fangari@gmail.com>
2013-06-07 13:56:29 -0500

    Reorganize the navigation menu for the site

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Simor::Application.routes.draw do
-root to: "contracts#index"
+root to: "assets#index"
 resources :contracts, :assets, :categories, :makes, :models,
-  :colors, :kinds, :bodies, :builds, :comments, :inspection_models
+  :colors, :kinds, :bodies, :builds, :comments, :inspections, :inspection_orders
 end
