3325a54e59ba6eee296ecd19244fcb993fe8e4f0
Fernando Angarita <fangari@gmail.com>
2013-06-09 17:06:17 -0500

    Refactors inspection factory code to use Faker generated data

--- spec/factories/inspections.rb
+++ spec/factories/inspections.rb
@@ -1,10 +1,11 @@
+require 'faker'
 FactoryGirl.define do
   factory :inspection do
     asset {create(:asset)}
     inspection_number 'V129'
-    address 'Carrera 1 # 2 - 3'
-    city 'Chicago'
-    state 'Ilinois'
+    address Faker::Address.street_address
+    city Faker::Address.city
+    state Faker::Address.state
     date Time.now
     current_value 10500000
     appraiser_value 10000000
@@ -14,20 +15,20 @@ FactoryGirl.define do
     gas_certificate '98798409882'
     gas_begin_date '2010-02-21'
     gas_finish_date '2012-02-21'
-    maintenance 'Yada yada yada yada'
-    repairs 'Yada yada yada yada'
-    security 'Yada yada yada yada'
-    exterior 'Yada yada yada yada'
-    interior 'Yada yada yada yada'
-    engine 'Yada yada yada yada'
-    accesories 'Yada yada yada yada'
+    maintenance Faker::Lorem.paragraph 10
+    repairs Faker::Lorem.paragraph 5
+    security Faker::Lorem.paragraph 5
+    exterior Faker::Lorem.paragraph 2
+    interior Faker::Lorem.paragraph 10
+    engine Faker::Lorem.paragraph 5
+    accesories Faker::Lorem.paragraph 10
     insurance_number '123UNOI98'
     insurance_company 'ING'
     insured_value 15000000
     currency 'COP'
     insurance_start '2013-05-24'
     insurance_finish '2014-05-24'
-    person_in_charge 'Tomas de Aquino'
+    person_in_charge Faker::Name.name
     pic_id '38.766.298'
     pic_job 'Security guard'
   end
