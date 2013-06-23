0f3b1ba64fb442415eb545cee0e5c0c511ad8f81
Fernando Angarita <fangari@gmail.com>
2013-06-23 16:35:57 -0500

    Adds attributes whitelist and validations to the Comments model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ActiveRecord::Base
-  attr_accessible :content
+  attr_accessible :content, :user_id, :commentable_id, :commentable_type
+  belongs_to :user
   belongs_to :commentable, polymorphic: true
+
+  validates :content, :commentable_id, :commentable_type, presence: true
 end
