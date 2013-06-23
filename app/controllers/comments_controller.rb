e2f96fae428e42926ce10ecd471135614e64aca9
Fernando Angarita <fangari@gmail.com>
2013-06-23 17:17:07 -0500

    Adds Comments controller and corresponding haml views.
    Adds Comment controller with only three actions to ensure that a comment will always have a parent, and once a comment is created it cannot be destroyed.
    
    Adds partial for one comment, and a partial for creating a comment. Both views are to be used by the parent commentable models.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,30 +1,4 @@
 class CommentsController < ApplicationController
-  def index
-    @comments = Comment.all
-
-    respond_to do |format|
-
-      format.json { render json: @comments }
-    end
-  end
-
-  def show
-    @comment = Comment.find(params[:id])
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @comment }
-    end
-  end
-
-  def new
-    @comment = Comment.new
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @comment }
-    end
-  end
 
   def edit
     @comment = Comment.find(params[:id])
@@ -32,39 +6,25 @@ class CommentsController < ApplicationController
 
   def create
     @comment = Comment.new(params[:comment])
-
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-        format.json { render json: @comment, status: :created, location: @comment }
+        format.html { redirect_to @comment.commentable, notice: 'Comment was successfully created.' }
       else
         format.html { render action: "new" }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
   end
 
   def update
     @comment = Comment.find(params[:id])
-
     respond_to do |format|
       if @comment.update_attributes(params[:comment])
-        format.html { redirect_to @comment, notice: 'Comment was successfully updated.' }
-        format.json { head :no_content }
+        format.html { redirect_to @comment.commentable, notice: 'Comment was successfully updated.' }
       else
         format.html { render action: "edit" }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  def destroy
-    @comment = Comment.find(params[:id])
-    @comment.destroy
 
-    respond_to do |format|
-      format.html { redirect_to comments_url }
-      format.json { head :no_content }
-    end
-  end
 end
