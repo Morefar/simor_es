0f1b8c17f393bf5496f4b29ef7569126395022c5
Fernando Angarita <fangari@gmail.com>
2013-07-03 16:21:30 -0500

    Adds Entity model specification and validations on the model.

--- app/models/cosigner.rb
+++ app/models/cosigner.rb
@@ -2,4 +2,6 @@ class Cosigner < ActiveRecord::Base
   attr_accessible :entity_id, :contract_id
   belongs_to :entity
   belongs_to :contract
+  validates :entity_id, :contract_id, presence: true
+  validates :entity_id, uniqueness: { scope: :contract_id }
 end
--- spec/models/cosigner_spec.rb
+++ spec/models/cosigner_spec.rb
@@ -6,8 +6,12 @@ describe Cosigner do
   it { should belong_to(:entity) }
   it { should allow_mass_assignment_of(:contract_id) }
   it { should allow_mass_assignment_of(:entity_id) }
+  it { should validate_presence_of(:entity_id) }
+  it { should validate_presence_of(:contract_id) }
 
   it 'has a valid factory' do
-    expect(build(:cosigner)).to be_valid
+    identification_type = create(:identification_type, name: "NIT")
+    entity = create(:entity, identification_type: identification_type)
+    expect(build(:cosigner, entity: entity)).to be_valid
   end
 end
