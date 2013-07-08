c941131d62e5f6ede8dfc2ca68d8af53c2afa09b
Fernando Angarita <fangari@gmail.com>
2013-07-08 10:17:10 -0500

    Updates validation on insurance company names.
    The validation was checking only for word characters, but sometimes the
    company names include non word characters such as "&", or ",".
    
    The factory was updated to generate valid nit identification numbers.

--- app/models/insurance_company.rb
+++ app/models/insurance_company.rb
@@ -4,7 +4,7 @@ class InsuranceCompany < ActiveRecord::Base
 
   validates :name, :nit, presence: true
   validates :name, :nit, uniqueness: true
-  validates :name, format: { with: /\A[\w\P{ASCII}\s?]+[a-z]+\Z/i }
+  validates :name, format: { with: /\A[\D]+[a-z.]+\Z/i }
   validates :nit, format: { with: /\A\d{3}([.]*)\d{3}\1\d{3}\Z/ }
   validates :nit, length: { within: 9..11 }
 end
--- spec/factories/insurance_companies.rb
+++ spec/factories/insurance_companies.rb
@@ -3,6 +3,6 @@ require 'faker'
 FactoryGirl.define do
   factory :insurance_company do
     name {"#{Faker::Company.name} #{Faker::Company.suffix}"}
-    sequence(:nit) { |n| "234.355.2#{n}"}
+    sequence(:nit) { |n| "234.355.2#{n+10}"}
   end
 end
