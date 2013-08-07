bc20a453747a301d48ae895eb78e19d69e863309
Fernando Angarita <fangari@gmail.com>
2013-08-07 17:55:32 -0500

    Refactors view code to DRY date fields on different forms.
    Instead of having different classes for every date field, the code is
    refactored to take advantage of jquery selectors to share the datepicker
    functionality across all date fields.

--- app/assets/javascripts/contracts.js
+++ app/assets/javascripts/contracts.js
@@ -1,9 +1,9 @@
 (function() {
   jQuery(function() {
     $.datepicker.setDefaults({
-      showOn: "button",
-      buttonImageOnly: true,
-      buttonImage: "/assets/calendar.png",
+      // showOn: "button",
+      // buttonImageOnly: true,
+      // buttonImage: "/assets/calendar.png",
       regional: "es",
       changeMonth: true,
       changeYear: true,
@@ -13,10 +13,7 @@
       duration: "slow",
       hideIfNoPrevNext: true
       });
-    $('#contract_start_date').datepicker();
-    $('#contract_first_canon_date').datepicker();
-    $('#contract_expiration_date').datepicker();
-    $('#contract_last_date_to_option').datepicker();
+    $('.date-field').datepicker();
   });
 }).call(this);
 
