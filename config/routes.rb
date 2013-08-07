8c495f298fecf972080c7521b2ccad8f2e8cc375
Fernando Angarita <fangari@gmail.com>
2013-08-07 17:05:46 -0500

    Adds route support to access Insurance Companies.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Simor::Application.routes.draw do
 root to: "assets#index"
 resources :contracts, :assets, :categories, :makes, :models,
-  :colors, :kinds, :bodies, :builds, :inspections, :inspection_orders
+  :colors, :kinds, :bodies, :builds, :inspections, :inspection_orders, :insurance_companies
 resources :comments, only: [:create, :update, :edit]
 resources :entities
 resources :documents, except: [:index]
