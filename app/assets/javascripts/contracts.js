c5d9f523048326581ba426cda2463436c5688a09
Fernando Angarita <fangari@gmail.com>
2013-08-10 06:44:44 -0500

    Adds ui behavior enhancements for the contract form.
    Adds functions to calculate a date base on a starting date and a given
    number of months to aid in calculating the contract expiry date. Adds a
    function that toggles the disabled state of a date field.

--- app/assets/javascripts/contracts.js
+++ app/assets/javascripts/contracts.js
@@ -1,5 +1,6 @@
+// Date Picker behavior.
 (function() {
-  jQuery(function() {
+  $(function() {
     $.datepicker.setDefaults({
       // showOn: "button",
       // buttonImageOnly: true,
@@ -17,10 +18,58 @@
   });
 }).call(this);
 
-var hiddenCommentBox = $('.new_comment');
-$('#new-comment-button').on('click', function( event ) {
+//Comment form behavior.
+$('#new-comment-button').on('click', displayCommentForm);
+function displayCommentForm() {
+  var hiddenCommentBox = $('.new_comment');
   $(this).hide('fast');
   hiddenCommentBox.show('slow');
   hiddenCommentBox.find('textarea').focus();
-});
+};
 
+// Toggle disable when option is checked.
+$("#contract_option_to_buy").change(function() {
+    var $input = $(this);
+    var $datefield = $('#contract_last_date_to_option');
+    if ($input.prop('checked')){
+      $datefield.prop('disabled', false);
+    } else {
+      $datefield.prop('disabled', true)
+    }
+}).change();
+
+// Date handling functions
+Date.isLeapYear = function (year) {
+  return (((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0));
+};
+
+Date.getDaysInMonth = function (year, month) {
+  return [31, (Date.isLeapYear(year) ? 29 : 28), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][month];
+};
+
+Date.prototype.isLeapYear = function () {
+  var y = this.getFullYear();
+  return (((y % 4 === 0) && (y % 100 !== 0)) || (y % 400 === 0));
+};
+
+Date.prototype.getDaysInMonth = function () {
+  return Date.getDaysInMonth(this.getFullYear(), this.getMonth());
+};
+
+Date.prototype.addMonths = function (value) {
+  var n = this.getDate();
+  this.setDate(1);
+  this.setMonth(this.getMonth() + value);
+  this.setDate(Math.min(n, this.getDaysInMonth()));
+  return this;
+};
+
+// Update expiration date based on first canon date and number of periods.
+$('#contract_duration').on('blur', function(){
+  var $duration = $(this).val();
+  var $periods = $('#contract_periodicity').val();
+  var $startDate = new Date($("#contract_first_canon_date").val());
+  var $months = ($duration * $periods);
+  var $endDate = $startDate.addMonths($months);
+  $('#contract_expiration_date').val($endDate.getFullYear()+'-'+$endDate.getMonth()+'-'+$endDate.getDate());
+});
