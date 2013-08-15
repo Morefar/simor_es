d0ff09f943a028a5d0777a20bebcb2caa4003dec
Fernando Angarita <fangari@gmail.com>
2013-08-15 10:40:38 -0500

    Adds translations and support for inspection views.
    Adds translation support and YAML files with the corresponding
    translations to all the inspection views.

--- app/helpers/inspections_helper.rb
+++ app/helpers/inspections_helper.rb
@@ -1,8 +1,2 @@
 module InspectionsHelper
-  def local_number_to_currency(number_attribute)
-    loc(number_attribute).to_currency.to_s(precision: 2)
-  end
-  def loc(attribute)
-    attribute.localize(I18n.locale)
-  end
 end
