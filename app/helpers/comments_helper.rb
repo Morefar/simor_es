5213e399dcbf48392ea12e074250bde605c8277d
Fernando Angarita <fangari@gmail.com>
2013-08-20 15:19:25 -0500

    Adds spanish support and translations for the comments views.

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,10 @@
 module CommentsHelper
+  def localize_time_ago_in_words(date)
+    case I18n.locale
+    when :en
+      return "#{time_ago_in_words(date)} ago"
+    when :es
+      return "Hace #{time_ago_in_words(date)}"
+    end
+  end
 end
