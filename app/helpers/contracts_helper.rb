0bac71960ca08fb9e11e387cea6e3a231880eab1
Fernando Angarita <fangari@gmail.com>
2013-08-20 10:05:56 -0500

    Adds spanish translations to the Contract views and minor upgrades.
    Adds spanish support and translations to the contract views, as well as
    minor refactorings to clean view code.

--- app/helpers/contracts_helper.rb
+++ app/helpers/contracts_helper.rb
@@ -1,2 +1,14 @@
 module ContractsHelper
+  def contract_periodicity(periodicity)
+    case periodicity
+    when 1
+      return t 'contracts.form.months'
+    when 3
+      return t 'contracts.form.quarters'
+    when 6
+      return t 'contracts.form.semesters'
+    when 12
+      return t 'contracts.form.years'
+    end
+  end
 end
