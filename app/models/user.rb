175b797fb7204194981fb0a4ce82c8b001a5a7ca
Fernando Angarita <fangari@gmail.com>
2013-09-29 13:48:39 -0500

    Adds Users model to use with devise.
    Adds authentication with Devise and it's supporting user model, on the
    routes file adds the routes for users and locks everything down by
    requiring that a user is authenticated in order to use any resource.

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, 
+         :confirmable, :lockable, :timeoutable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :first_name, :last_name, :avatar, :email, :password, :password_confirmation, :remember_me, :account_name
+  belongs_to :account
+
+  def account_name=(account_name)
+    self.account = Account.find_by_name(account_name) if account_name?
+  end
+  def account_name
+    account.try(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Simor::Application.routes.draw do
   scope '(:locale)' do
-    root to: "assets#index"
-    resources :contracts, :assets, :categories, :makes, :models, :colors, :kinds, :bodies, :builds, :inspections, :inspection_orders, :insurance_companies
+    devise_for :users
+    authenticated do
     resources :comments, only: [:create, :update, :edit]
     resources :entities
     resources :documents, except: [:index]
+    end
   end
 end
