8c18ea51838bb318803465bc3bb3995da46b2d40
Fernando Angarita <fangari@gmail.com>
2013-09-29 14:44:31 -0500

    Changes the landing pages for authenticated and unathenticated users.
    Changes the routes file so that authenticated users get a landing page
    withing the app, and the unathenticated user get sent to the landing
    page.
    
    It adds two controllers a dashboard controllers that will be in charge
    of getting the information to render an information dashboard, and a
    pages controller that will be in charge of the static pages.

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Simor::Application.routes.draw do
   scope '(:locale)' do
     devise_for :users
     authenticated do
-    resources :comments, only: [:create, :update, :edit]
-    resources :entities
-    resources :documents, except: [:index]
+      root to: "Dashboard#show"
+      resource :dashboard
+      resources :contracts, :assets, :categories, :makes, :models, :colors, :kinds, :bodies, :builds, :inspections, :inspection_orders, :insurance_companies
+      resources :comments, only: [:create, :update, :edit]
+      resources :entities
+      resources :documents, except: [:index]
     end
+    root to: 'Pages#landing'
   end
 end
