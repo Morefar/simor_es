51e8dee2c977d1917e8e1bb248b2cb9df785c449
Fernando Angarita <fangari@gmail.com>
2013-10-01 18:57:17 -0500

    Changes configuration so that secrets are used thru env variables.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,8 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Simor::Application.config.secret_token = 'a977962ea47b93b2e9254fac873e975f4bcf0e843eab119ad0350c4735194433fdce51af63902b6286a5d0503c2f65b91ca8991d75a4fddd5ed0c63a4f5550f4'
+Simor::Application.config.secret_token = if Rails.env.development? or Rails.env.test?
+    'a977962ea47b93b2e9254fac873e975f4bcf0e843eab119ad0350c4735194433fdce51af63902b6286a5d0503c2f65b91ca8991d75a4fddd5ed0c63a4f5550f4'
+  else
+    ENV['SECRET_TOKEN']
+  end
