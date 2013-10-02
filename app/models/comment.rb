5289e86bc51cb2cfb00f336d1d065192e56d443f
Fernando Angarita <fangari@gmail.com>
2013-10-02 16:19:48 -0500

    Adds association between users and comments.
    This relationship ensures that when a user adds a comment, that comment
    gets saved and related to his history.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,9 @@
 class Comment < ActiveRecord::Base
-  attr_accessible :content, :user_id, :commentable_id, :commentable_type
+  attr_accessible :content, :user_id, :commentable_id, :commentable_type, :user_id
   belongs_to :user
   belongs_to :commentable, polymorphic: true
 
   validates :content, :commentable_id, :commentable_type, presence: true
+
+  default_scope order("created_at DESC")
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,20 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, 
+         :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable, :timeoutable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :first_name, :last_name, :avatar, :email, :password, :password_confirmation, :remember_me, :account_name
+  attr_accessible :first_name, :last_name, :avatar, :email, :password,
+    :password_confirmation, :remember_me, :account_name
   belongs_to :account
+  has_many :comments
 
   def account_name=(account_name)
   end
   def account_name
+  end
+  def full_name
+    "#{first_name} #{last_name}"
   end
 end
