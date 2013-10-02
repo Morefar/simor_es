47f40b9781c9b571275452803d49728818443445
Fernando Angarita <fangari@gmail.com>
2013-10-02 09:12:57 -0500

    Adds authentication constraints to access models.
    The app requires authentication in order to access any resource, at the same time declares two root paths depending on the
    authentication state of the user.

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,14 @@ Simor::Application.routes.draw do
     devise_for :users
     authenticated do
       root to: "Dashboard#show"
+    end
+    authenticate do
       resource :dashboard
       resources :contracts, :assets, :categories, :makes, :models, :colors, :kinds, :bodies, :builds, :inspections, :inspection_orders, :insurance_companies
       resources :comments, only: [:create, :update, :edit]
       resources :entities
       resources :documents, except: [:index]
     end
-    root to: 'Pages#landing'
+  root to: 'Pages#landing'
   end
 end
