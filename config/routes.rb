81e636cb37ae14d5f9f3c011638e853b8f16597a
Fernando Angarita <fangari@gmail.com>
2014-01-05 15:13:21 -0500

    Upgrades routes file to make it compatible with Rails 4.0.2

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@ Simor::Application.routes.draw do
   scope '(:locale)' do
     devise_for :users
     authenticated do
-      root to: "Dashboard#show"
+      get '/', to: "dashboard#show", as: :user_root
     end
     authenticate do
-      resource :dashboard
+      resource :dashboardi, only: [:show]
       resources :contracts, :assets, :categories, :makes, :models, :colors, :kinds, :bodies, :builds, :inspections, :inspection_orders, :insurance_companies
       resources :comments, only: [:create, :update, :edit]
       resources :entities
       resources :documents, except: [:index]
     end
-  root to: 'Pages#landing'
+    root to: 'pages#landing'
   end
 end
