a63992c0bcfd355166053f43db93749a9d8f652f
Fernando Angarita <fangari@gmail.com>
2014-02-07 10:59:02 -0500

    Fixes type with production mail configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,8 +84,8 @@ Simor::Application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default = { charset: "utf-8" }
-  config.action_mailer.stmp_settings = {
-    address: "stmp.gmail.com",
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
     port: 587,
     domain: ENV['GMAIL_DOMAIN'],
     authentication: "plain",
