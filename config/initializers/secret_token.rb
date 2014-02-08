d9b09a9aa320bca4b120bf45794de36a5a752506
Fernando Angarita <fangari@gmail.com>
2014-02-08 07:01:42 -0500

    Fixes issue with Rails4, Devise and Heroku
    For some reason Heroku calls on the secret_token initializer during
    asset precompilation, so according to their docs the nil case must be
    handled gracefully.
    https://devcenter.heroku.com/articles/rails-asset-pipeline#troubleshooting

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,8 +9,13 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Simor::Application.config.secret_key_base = if Rails.env.development? or Rails.env.test?
+secret = if Rails.env.development? or Rails.env.test?
     'ad30759f4c01dd0536dd1e5249b6abc81ba9251fb3f816784979456d554754e956c3a37ffd90f1d84109d56d0316867cd9b8469ec51fe7e9fd27256a5bfc0852'
   else
     ENV['SECRET_TOKEN']
   end
+if secret.nil? || secret.length < 30
+  puts "Secret token cannot be loaded"
+else
+  Simor::Application.config.secret_key_base = secret
+end
