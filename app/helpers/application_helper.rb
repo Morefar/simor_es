6ae0e5753ed84faa6d1a53cbefef9e17dad28f49
Fernando Angarita <fangari@gmail.com>
2014-02-10 14:01:48 -0500

    Adds helper method to create sidebar buttons
    Adds a helper method to cleanup duplication of code when creating new
    sidebar buttons. Contract views make use of such a helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,16 @@
 module ApplicationHelper
   def local_number_to_currency(number_attribute)
-    loc(number_attribute).to_currency.to_s(precision: 2)
+    loc(number_attribute).to_currency.to_s(precision: 2) unless number_attribute.nil?
   end
   def loc(attribute)
     attribute.localize("#{ I18n.locale }_co")
+  end
+  def sidebar_button(options = {})
+    content_tag(:li, class: "btn-tny inactive") do
+      link_to options[:url] do
+        %{ #{ options[:label] }
+        <i class="icon-#{options[:icon]} task-icon icon-large"></i> }.html_safe
+      end
+    end
   end
 end
