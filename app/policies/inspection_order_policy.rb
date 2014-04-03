20441672b846ad3be21985dc1a57eeba683ca45b
Fernando Angarita <fangari@gmail.com>
2014-04-03 12:27:14 -0500

    Adds rule so to prevent inspection order change when already inspected.

--- app/policies/inspection_order_policy.rb
+++ app/policies/inspection_order_policy.rb
@@ -1,4 +1,12 @@
 class InspectionOrderPolicy < ApplicationPolicy
+  def edit?
+    if super
+      !record.inspected?
+    end
+  end
+  def update?
+    edit?
+  end
   self::Scope = Struct.new(:user, :scope) do
     def resolve
       scope
