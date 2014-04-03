18673b4f68b24fed7987164bba0bf63e94e93084
Fernando Angarita <fangari@gmail.com>
2014-04-03 15:07:56 -0500

    Adds style organization and includes policy class definition.

--- app/models/model.rb
+++ app/models/model.rb
@@ -14,11 +14,23 @@
 #
 
 class Model < ActiveRecord::Base
+  #-- Relationships --------------------
   belongs_to :make, inverse_of: :models
+
+  #-- Delegations ----------------------
   delegate :name, to: :make, prefix: true, allow_nil: true
+
+  #-- Scopes ---------------------------
   default_scope { order(:make_id, :name) }
   scope :search_name, ->(name) { where("name ilike ?", name) }
+
+  #-- Validations ----------------------
   validates :name, presence: true
   validates :name, format: { with: /\A[\p{L}\p{P}\+\d\s]+\Z/iu }
   validates :name, uniqueness: { scope: :make_id }
+
+  #-- Class methods --------------------
+  def self.policy_class
+    ModelPolicy
+  end
 end
