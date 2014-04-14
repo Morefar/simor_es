eb67632daf06b84960b2e32f07308b61077d3887
Fernando Angarita <fangari@gmail.com>
2014-04-14 09:47:33 -0500

    Modifies controllers to use the activities being tracked.
    The application controller was modified to set the current user as the
    owner of the activities being tracked. The dashboard controller was
    modified to keep an array of activities to be displayed on the views.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,7 @@ class DashboardController < ApplicationController
   skip_after_action :verify_authorized
 
   def show
+    @activities = PublicActivity::Activity.order(created_at: :desc).limit(20)
     @user = current_user
   end
 end
